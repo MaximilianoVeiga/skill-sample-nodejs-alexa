d8342f3b768356737dd20e5a783e7fa34f2d6c48
Maximiliano Neto <maxsantosneto@gmail.com>
2021-01-07 17:11:33 -0300

    Change welcome.js

--- lambda/custom/responses/welcome.js
+++ lambda/custom/responses/welcome.js
@@ -1,10 +1,32 @@
-module.exports = {
-    messages: {
-        WELCOME_MSG: `<speak> Olá! </speak>`,
-        HELLO_MSG: `<speak> Seja bem-vindo ao Hello World. </speak>`,
-        HELP_MSG: `<speak> Bem-vindo a ajuda. </speak>`,
-        GOODBYE_MSG: `<speak> Até mais. </speak>`,
-        FALLBACK_MSG: `<speak> Desculpe, não entendi. </speak>`,
-        ERROR_MSG: `<speak> Um erro ocorreu nesta skill. </speak>`
-    }
-}
+function WELCOME_MSG() {​​
+    return `<speak> Olá! </speak>`;
+}​​
+
+function HELLO_MSG() {​​
+    return `<speak> Seja bem-vindo ao Hello World. </speak>`;
+}​​
+
+function HELP_MSG() {​​
+    return `<speak> Bem-vindo a ajuda. </speak>`;
+}​​
+
+function GOODBYE_MSG() {​​
+    return `<speak> Até mais. </speak>`;
+}​​
+
+function FALLBACK_MSG() {​​
+    return `<speak> Desculpe, não entendi. </speak>`;
+}​​
+
+function ERROR_MSG() {​​
+    return `<speak> Um erro ocorreu nesta skill. </speak>`;
+}​​
+
+module.exports = {​​
+    WELCOME_MSG,
+    HELLO_MSG,
+    HELP_MSG,
+    GOODBYE_MSG,
+    FALLBACK_MSG,
+    ERROR_MSG
+}​​
