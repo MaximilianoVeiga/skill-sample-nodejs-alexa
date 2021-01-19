5849cfbb94beeecd25df290ce320e1607943acc6
Maximiliano Veiga <53234944+MaximilianoVeiga@users.noreply.github.com>
2021-01-19 14:13:34 -0300

    Update welcome.js

--- lambda/custom/responses/welcome.js
+++ lambda/custom/responses/welcome.js
@@ -1,32 +1,32 @@
-function WELCOME_MSG() {​​
+function WELCOME_MSG() {
     return `<speak> Olá! </speak>`;
-}​​
+}
 
-function HELLO_MSG() {​​
+function HELLO_MSG() {
     return `<speak> Seja bem-vindo ao Hello World. </speak>`;
-}​​
+}
 
-function HELP_MSG() {​​
+function HELP_MSG() {
     return `<speak> Bem-vindo a ajuda. </speak>`;
-}​​
+}
 
-function GOODBYE_MSG() {​​
+function GOODBYE_MSG() {
     return `<speak> Até mais. </speak>`;
-}​​
+}
 
-function FALLBACK_MSG() {​​
+function FALLBACK_MSG() {
     return `<speak> Desculpe, não entendi. </speak>`;
-}​​
+}
 
-function ERROR_MSG() {​​
+function ERROR_MSG() {
     return `<speak> Um erro ocorreu nesta skill. </speak>`;
-}​​
+}
 
-module.exports = {​​
+module.exports = {
     WELCOME_MSG,
     HELLO_MSG,
     HELP_MSG,
     GOODBYE_MSG,
     FALLBACK_MSG,
     ERROR_MSG
-}​​
+}
